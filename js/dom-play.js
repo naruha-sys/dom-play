7cb4779e84181dae593ddd48642671073f7129a4
Uran <106543523+naruha-sys@users.noreply.github.com>
2026-05-28 14:00:13 -0700

    updates elements' color

--- js/dom-play.js
+++ js/dom-play.js
@@ -1,2 +1,9 @@
-alert("Hello from dom-play.js!");
-      
+const spans = document.querySelectorAll("#play span");
+console.log(spans);
+
+
+for (const mySpan of spans) {
+    mySpan.style.backgroundColor = "yellow";
+    let actor = mySpan.dataset.actor;
+    alert(actor);
+}
