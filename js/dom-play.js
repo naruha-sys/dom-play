85b3a02595b86bae6d4d66d7cd9289ae6cc6af69
Uran <106543523+naruha-sys@users.noreply.github.com>
2026-05-28 15:58:19 -0700

    add comments

--- js/dom-play.js
+++ js/dom-play.js
@@ -1,9 +1,25 @@
+/*
+
+
+   Create an app that on click actors lines are highlighted
+
+
+   With a single click, highlight (bold or otherwise) all lines for a specific actor in a play
+   Un-highlight (normal or otherwise) all lines from all other actors in the play
+  
+   The function(s) created to do the work must accommodate any number of actors, and any number of additional lines added to the page
+  
+   HTML "hooks" may be added to the page for proper operation
+
+
+*/
+
 const spans = document.querySelectorAll("#play span");
 console.log(spans);
 
 
 for (const mySpan of spans) {
-    mySpan.style.backgroundColor = "yellow";
+    //mySpan.style.backgroundColor = "yellow";
     let actor = mySpan.dataset.actor;
-    alert(actor);
+    
 }
